c0af4b4ae4ccdd95834a44e1c651ab3950327f3c
Diffelentor <Diffelentor@gmail.com>
2022-12-05 19:13:19 +0800

    2022/12/05/19:13/zht

--- src/exchanges/dao/GetExchangesData.java
+++ src/exchanges/dao/GetExchangesData.java
@@ -1,4 +1,4 @@
-package shares.dao;
+package exchanges.dao;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
@@ -18,7 +18,7 @@ import java.util.TimerTask;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class GetSharesData implements ServletContextListener{
+public class GetExchangesData implements ServletContextListener{
     private Timer timer = null;
     private TimerTask task = null;
     ServletContext data;
@@ -29,30 +29,29 @@ public class GetSharesData implements ServletContextListener{
 
     @Override
     public void contextInitialized(ServletContextEvent context) {
-
         data = context.getServletContext();
         // 上下文初始化执行
         //System.out.println("================>[ServletContextListener]自动加载启动开始.");
         timer = new Timer(true);
-        Date date = (new Date());
-        int period = 30 * 1000;
         MyTimerTask task = new MyTimerTask();
-        timer.schedule(task,2000,10000); //延迟2s，每隔20s执行
-        System.out.println("[GetSharesData]运行了");
+        timer.schedule(task,2000,10000); //延迟2s，每隔10s执行
+        System.out.println("[GetExchangesData]运行了");
     }
 
     public class MyTimerTask extends TimerTask{
+        private StringBuffer out;
         @Override
         public void run(){
             Calendar calendar = Calendar.getInstance();
-            /*目标股票列表*/
-            String[] shares_list = new String[]{"sh000001","sz399001","sh000300","sz399415","sz399006","sh600000","sh600015","sh601318","sh601628","sh601988","sh600028","sh601857","sh600055","sz300015","sz300396","sh600085","sh600129","sh600332","sz000538","sh600519","sh600132","sz000858","sh600048","sh600606","sz000002"};
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-DD HH:mm:ss");
+            /*目标外汇列表*/
+            String[] exchanges_list = new String[]{"USDCNY","EURCNY","CNYJPY","GBPCNY","HKDCNY","DINIW"};
 
             System.out.println("Current Time："+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));//获取当前系统时间
-            System.out.println("开始定时获取股票数据");
+            System.out.println("开始定时获取外汇数据");
             try {
-                for (String share_id : shares_list){
-                    GetShares_and_store(share_id);
+                for (String exchange_id : exchanges_list){
+                    GetExchanges_and_store(exchange_id);
                 }
             } catch (SQLException | IOException e) {
                 throw new RuntimeException(e);
@@ -60,12 +59,12 @@ public class GetSharesData implements ServletContextListener{
         }
     }
 
-    private void GetShares_and_store(String share_id) throws SQLException, IOException {
+    private void GetExchanges_and_store(String exchange_id) throws SQLException, IOException {
         String dbname = "test";
         Db updateDb = new Db(dbname);
 
-        // 1. 获取访问地址URL
-        URL url = new URL("https://hq.sinajs.cn/list=" + share_id);
+// 1. 获取访问地址URL
+        URL url = new URL("https://hq.sinajs.cn/list=" + exchange_id);
         // 2. 创建HttpURLConnection对象
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         /* 3. 设置请求参数等 */
@@ -105,20 +104,18 @@ public class GetSharesData implements ServletContextListener{
         String[] infoArray = msg.split(",");
         //System.out.println(infoArray[0]);
 
-        String name = infoArray[0];
-        double price_today_begin = Double.valueOf(infoArray[1]);
-        double price_yesterday = Double.valueOf(infoArray[2]);
-        double price_right_now = Double.valueOf(infoArray[3]);
-        double price_high = Double.valueOf(infoArray[4]);
-        double price_low = Double.valueOf(infoArray[5]);
-        double deal_count = Double.valueOf(infoArray[8]);
-        double deal_amount = Double.valueOf(infoArray[9]);
-        String time = infoArray[30] + " " + infoArray[31];
-        String date = infoArray[30];
-        //System.out.println("执行到这里");
+        String name = infoArray[9];
+        String time = infoArray[10] + " " + infoArray[0];
+        String date = infoArray[10];
+        double price_today_begin = Double.valueOf(infoArray[5]);
+        double price_yesterday = Double.valueOf(infoArray[6]);
+        double price_right_now = Double.valueOf(infoArray[1]);
+        double price_high = Double.valueOf(infoArray[7]);
+        double price_low = Double.valueOf(infoArray[8]);
+
         String sql = "";
         int count = 0;
-        String check_if_exist = "select count(*) as total from shares where shares_id = '" + share_id +"' and date = '" + date +"'";
+        String check_if_exist = "select count(*) as total from exchanges where exchanges_id = '" + exchange_id +"' and date = '" + date +"'";
         try{
             ResultSet rs = updateDb.executeQuery(check_if_exist);
             while(rs.next()){
@@ -128,29 +125,28 @@ public class GetSharesData implements ServletContextListener{
         }catch(Exception e){
             System.out.println(e);
         }
-
+        /*第一次录入则插入记录*/
         if (count == 0){
-            sql = "insert into shares(shares_id,shares_name,price_today_begin,price_pre,price_right_now,price_high,price_low,deal_count,deal_amount,select_time,date)";
-            sql += " values('" + share_id + "'" + " ,'" + name + "'" + " ,'" + price_today_begin + "'" + " ,'" + price_yesterday + "'" + " ,'" + price_right_now + "'" + " ,'" + price_high + "'" + " ,'" + price_low + "'" + " ,'" + deal_count + "'" + " ,'" + deal_amount + "'" + " ,'" + time + "'" +" ,'"+date+"')";
+            sql = "insert into exchanges(exchanges_id,exchanges_name,price_today_begin,price_yesterday,price_right_now,price_high,price_low,select_time,date)";
+            sql += " values('" + exchange_id + "'" + " ,'" + name + "'" + " ,'" + price_today_begin + "'" + " ,'" + price_yesterday + "'" + " ,'" + price_right_now + "'" + " ,'" + price_high + "'" + " ,'" + price_low + "'" + " ,'" + time + "'" +" ,'" + date +"')";
         }
+        /*非第一次则修改记录*/
         else{
-            sql = "update shares set price_today_begin='"+price_today_begin+"'";
-            sql += " ,price_pre='"+price_yesterday+"'";
+            sql = "update exchanges set price_today_begin='"+price_today_begin+"'";
+            sql += " ,price_yesterday='"+price_yesterday+"'";
             sql += " ,price_right_now='"+price_right_now+"'";
             sql += " ,price_high='"+price_high+"'";
             sql += " ,price_low='"+price_low+"'";
-            sql += " ,deal_count='"+deal_count+"'";
-            sql += " ,deal_amount='"+deal_amount+"'";
             sql += " ,select_time='"+time+"'";
-            sql += " where shares_id='"+ share_id +"'";
+            sql += " where exchanges_id='"+ exchange_id +"'";
             sql += " and date='" + date + "'";
         }
         updateDb.executeUpdate(sql);
 
-        /*写入total*/
+        /*total表*/
         String sql_total = "";
         count = 0;
-        String check_if_exist_total = "select count(*) as total from total where futures_id = '" + share_id +"' and date = '" + date +"'";
+        String check_if_exist_total = "select count(*) as total from total where futures_id = '" + exchange_id +"' and date = '" + date +"'";
         try{
             ResultSet rs = updateDb.executeQuery(check_if_exist_total);
             while(rs.next()){
@@ -162,8 +158,8 @@ public class GetSharesData implements ServletContextListener{
         }
         /*第一次录入则插入记录*/
         if (count == 0){
-            sql_total = "insert into total(futures_id,futures_name,type,price_today_begin,price_yesterday,price_right_now,price_high,price_low,deal_count,deal_amount,select_time,date)";
-            sql_total += " values('" + share_id + "'" + " ,'" + name + "'" + " ,'股票' ,'" + price_today_begin + "'" + " ,'" + price_yesterday + "'" + " ,'" + price_right_now + "'" + " ,'" + price_high + "'" + " ,'" + price_low + "' ,'" + deal_count + "'" + " ,'" + deal_amount + "'" + " ,'" + time + "'" +" ,'" + date +"')";
+            sql_total = "insert into total(futures_id,futures_name,type,price_today_begin,price_yesterday,price_right_now,price_high,price_low,select_time,date)";
+            sql_total += " values('" + exchange_id + "'" + " ,'" + name + "'" + " ,'外汇' ,'" + price_today_begin + "'" + " ,'" + price_yesterday + "'" + " ,'" + price_right_now + "'" + " ,'" + price_high + "'" + " ,'" + price_low + "'" + " ,'" + time + "'" +" ,'" + date +"')";
         }
         /*非第一次则修改记录*/
         else{
@@ -172,10 +168,8 @@ public class GetSharesData implements ServletContextListener{
             sql_total += " ,price_right_now='"+price_right_now+"'";
             sql_total += " ,price_high='"+price_high+"'";
             sql_total += " ,price_low='"+price_low+"'";
-            sql_total += " ,deal_count='"+deal_count+"'";
-            sql_total += " ,deal_amount='"+deal_amount+"'";
             sql_total += " ,select_time='"+time+"'";
-            sql_total += " where futures_id='"+ share_id +"'";
+            sql_total += " where futures_id='"+ exchange_id +"'";
             sql_total += " and date='" + date + "'";
         }
         updateDb.executeUpdate(sql_total);
--- src/futures/dao/GetFuturesData.java
+++ src/futures/dao/GetFuturesData.java
@@ -1,4 +1,4 @@
-package shares.dao;
+package futures.dao;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
@@ -18,7 +18,7 @@ import java.util.TimerTask;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class GetSharesData implements ServletContextListener{
+public class GetFuturesData implements ServletContextListener{
     private Timer timer = null;
     private TimerTask task = null;
     ServletContext data;
@@ -38,21 +38,23 @@ public class GetSharesData implements ServletContextListener{
         int period = 30 * 1000;
         MyTimerTask task = new MyTimerTask();
         timer.schedule(task,2000,10000); //延迟2s，每隔20s执行
-        System.out.println("[GetSharesData]运行了");
+        System.out.println("[GetFuturesData]运行了");
     }
 
     public class MyTimerTask extends TimerTask{
+        private StringBuffer out;
         @Override
         public void run(){
             Calendar calendar = Calendar.getInstance();
-            /*目标股票列表*/
-            String[] shares_list = new String[]{"sh000001","sz399001","sh000300","sz399415","sz399006","sh600000","sh600015","sh601318","sh601628","sh601988","sh600028","sh601857","sh600055","sz300015","sz300396","sh600085","sh600129","sh600332","sz000538","sh600519","sh600132","sz000858","sh600048","sh600606","sz000002"};
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-DD HH:mm:ss");
+            /*目标期货列表*/
+            String[] futures_list = new String[]{"SC2301","TA2301","MA2301","RB2301","AL2301","I2301","V2301","P2301","M2301","Y2301","FU2301","AU0","AG0","CU2301"};
 
             System.out.println("Current Time："+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));//获取当前系统时间
-            System.out.println("开始定时获取股票数据");
+            System.out.println("开始定时获取期货数据");
             try {
-                for (String share_id : shares_list){
-                    GetShares_and_store(share_id);
+                for (String future_id :futures_list){
+                    GetFutures_and_store(future_id);
                 }
             } catch (SQLException | IOException e) {
                 throw new RuntimeException(e);
@@ -60,12 +62,12 @@ public class GetSharesData implements ServletContextListener{
         }
     }
 
-    private void GetShares_and_store(String share_id) throws SQLException, IOException {
+    private void GetFutures_and_store(String future_id) throws SQLException, IOException {
         String dbname = "test";
         Db updateDb = new Db(dbname);
 
-        // 1. 获取访问地址URL
-        URL url = new URL("https://hq.sinajs.cn/list=" + share_id);
+// 1. 获取访问地址URL
+        URL url = new URL("https://hq.sinajs.cn/list=" + future_id);
         // 2. 创建HttpURLConnection对象
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         /* 3. 设置请求参数等 */
@@ -106,19 +108,21 @@ public class GetSharesData implements ServletContextListener{
         //System.out.println(infoArray[0]);
 
         String name = infoArray[0];
-        double price_today_begin = Double.valueOf(infoArray[1]);
-        double price_yesterday = Double.valueOf(infoArray[2]);
-        double price_right_now = Double.valueOf(infoArray[3]);
-        double price_high = Double.valueOf(infoArray[4]);
-        double price_low = Double.valueOf(infoArray[5]);
-        double deal_count = Double.valueOf(infoArray[8]);
-        double deal_amount = Double.valueOf(infoArray[9]);
-        String time = infoArray[30] + " " + infoArray[31];
-        String date = infoArray[30];
-        //System.out.println("执行到这里");
+        double price_today_begin = Double.valueOf(infoArray[2]);
+        double price_yesterday = Double.valueOf(infoArray[5]);
+        double price_right_now = Double.valueOf(infoArray[8]);
+        double price_high = Double.valueOf(infoArray[3]);
+        double price_low = Double.valueOf(infoArray[4]);
+        double deal_price = Double.valueOf(infoArray[9]);
+        double deal_price_yesterday = Double.valueOf(infoArray[10]);
+        double buy_amount = Double.valueOf(infoArray[11]);
+        double sell_amount = Double.valueOf(infoArray[12]);
+        String time = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+
         String sql = "";
         int count = 0;
-        String check_if_exist = "select count(*) as total from shares where shares_id = '" + share_id +"' and date = '" + date +"'";
+        String check_if_exist = "select count(*) as total from futures where futures_id = '" + future_id +"' and date = '" + date +"'";
         try{
             ResultSet rs = updateDb.executeQuery(check_if_exist);
             while(rs.next()){
@@ -130,27 +134,30 @@ public class GetSharesData implements ServletContextListener{
         }
 
         if (count == 0){
-            sql = "insert into shares(shares_id,shares_name,price_today_begin,price_pre,price_right_now,price_high,price_low,deal_count,deal_amount,select_time,date)";
-            sql += " values('" + share_id + "'" + " ,'" + name + "'" + " ,'" + price_today_begin + "'" + " ,'" + price_yesterday + "'" + " ,'" + price_right_now + "'" + " ,'" + price_high + "'" + " ,'" + price_low + "'" + " ,'" + deal_count + "'" + " ,'" + deal_amount + "'" + " ,'" + time + "'" +" ,'"+date+"')";
+            sql = "insert into futures(futures_id,futures_name,price_today_begin,price_yesterday,price_right_now,price_high,price_low,deal_price,deal_price_yesterday,buy_amount,sell_amount,select_time,date)";
+            sql += " values('" + future_id + "'" + " ,'" + name + "'" + " ,'" + price_today_begin + "'" + " ,'" + price_yesterday + "'" + " ,'" + price_right_now + "'" + " ,'" + price_high + "'" + " ,'" + price_low + "'" + " ,'" + deal_price + "'" + " ,'" + deal_price_yesterday + "'"+ " ,'" + buy_amount + "'"+ " ,'" + sell_amount + "'" + " ,'" + time + "'"+" ,'" +date+"')";
         }
         else{
-            sql = "update shares set price_today_begin='"+price_today_begin+"'";
-            sql += " ,price_pre='"+price_yesterday+"'";
+            sql = "update futures set price_today_begin='"+price_today_begin+"'";
+            sql += " ,price_yesterday='"+price_yesterday+"'";
             sql += " ,price_right_now='"+price_right_now+"'";
             sql += " ,price_high='"+price_high+"'";
             sql += " ,price_low='"+price_low+"'";
-            sql += " ,deal_count='"+deal_count+"'";
-            sql += " ,deal_amount='"+deal_amount+"'";
+            sql += " ,deal_price='"+deal_price+"'";
+            sql += " ,deal_price_yesterday='"+deal_price_yesterday+"'";
+            sql += " ,buy_amount='"+buy_amount+"'";
+            sql += " ,sell_amount='"+sell_amount+"'";
             sql += " ,select_time='"+time+"'";
-            sql += " where shares_id='"+ share_id +"'";
+            sql += " where futures_id='"+ future_id +"'";
             sql += " and date='" + date + "'";
         }
         updateDb.executeUpdate(sql);
 
         /*写入total*/
+
         String sql_total = "";
         count = 0;
-        String check_if_exist_total = "select count(*) as total from total where futures_id = '" + share_id +"' and date = '" + date +"'";
+        String check_if_exist_total = "select count(*) as total from total where futures_id = '" + future_id +"' and date = '" + date +"'";
         try{
             ResultSet rs = updateDb.executeQuery(check_if_exist_total);
             while(rs.next()){
@@ -162,8 +169,8 @@ public class GetSharesData implements ServletContextListener{
         }
         /*第一次录入则插入记录*/
         if (count == 0){
-            sql_total = "insert into total(futures_id,futures_name,type,price_today_begin,price_yesterday,price_right_now,price_high,price_low,deal_count,deal_amount,select_time,date)";
-            sql_total += " values('" + share_id + "'" + " ,'" + name + "'" + " ,'股票' ,'" + price_today_begin + "'" + " ,'" + price_yesterday + "'" + " ,'" + price_right_now + "'" + " ,'" + price_high + "'" + " ,'" + price_low + "' ,'" + deal_count + "'" + " ,'" + deal_amount + "'" + " ,'" + time + "'" +" ,'" + date +"')";
+            sql_total = "insert into total(futures_id,futures_name,type,price_today_begin,price_yesterday,price_right_now,price_high,price_low,select_time,date)";
+            sql_total += " values('" + future_id + "'" + " ,'" + name + "'" + " ,'期货' ,'" + price_today_begin + "'" + " ,'" + price_yesterday + "'" + " ,'" + price_right_now + "'" + " ,'" + price_high + "'" + " ,'" + price_low + "'" + " ,'" + time + "'" +" ,'" + date +"')";
         }
         /*非第一次则修改记录*/
         else{
@@ -172,13 +179,12 @@ public class GetSharesData implements ServletContextListener{
             sql_total += " ,price_right_now='"+price_right_now+"'";
             sql_total += " ,price_high='"+price_high+"'";
             sql_total += " ,price_low='"+price_low+"'";
-            sql_total += " ,deal_count='"+deal_count+"'";
-            sql_total += " ,deal_amount='"+deal_amount+"'";
             sql_total += " ,select_time='"+time+"'";
-            sql_total += " where futures_id='"+ share_id +"'";
+            sql_total += " where futures_id='"+ future_id +"'";
             sql_total += " and date='" + date + "'";
         }
         updateDb.executeUpdate(sql_total);
+
         updateDb.close();
     }
 }
